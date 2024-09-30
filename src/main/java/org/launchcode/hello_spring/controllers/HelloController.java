a680bdcf606fc25e1a41e366deed2d3dff0065ad
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-30 17:48:01 -0500

    CT edit - Applied a class effect for RequestMapping to begin route with /hello

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
+@RequestMapping("hello")                                        // Every single method within this class should BEGIN route with "/hello" - And anything after is an additional path added
 public class HelloController {
 
     // Handles request at path /hello
 //    @GetMapping("hello")                                     // Tells SpringBoot that this method will ONLY accept GET requests
-//    @ResponseBody                                   // Tells SpringBoot that this method will return a plain text response
+//    @ResponseBody                                            // Tells SpringBoot that this method will return a plain text response
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
 
-    // Handles request at path /goodbye
+    // Handles request at path /hello/goodbye
     @GetMapping("goodbye")                                     // Tells SpringBoot that this method will ONLY accept GET requests
 //    @ResponseBody                                              // Tells SpringBoot that this method will return a plain text response
     public String goodbye() {
@@ -26,14 +27,14 @@ public class HelloController {
 
 
     /**** @RequestMapping Example ****/
-    // Responds to GET and POST requests at /hellogoodbye
+    // Responds to GET and POST requests at /hello/hellogoodbye
     @RequestMapping (value = "hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST})
     public String hellogoodbye() {
         return "Hello & Goodbye!";
     }
 
     // Responds to GET and POST requests at /hello
-    @RequestMapping (method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
+    @RequestMapping (method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
@@ -49,7 +50,7 @@ public class HelloController {
 //        return "Hello, " + name + "!";
 //    }
 
-    // Handles request of the form /launchcode?coder=LaunchCode
+    // Handles request of the form /hello/launchcode?coder=LaunchCode
     @GetMapping ("launchcode")
     public String launchcodeWithQueryParam(@RequestParam String coder) {
         return "Hello, " + coder + " is a student at LaunchCode!";
@@ -59,8 +60,8 @@ public class HelloController {
 
     /**** @PathParam Examples ****/
 
-    // Handles request od the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
+    // Handles request of the form /hello/LaunchCode
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
@@ -70,11 +71,12 @@ public class HelloController {
 
     /**** Forms Examples ****/
 
+    // Route is at /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-//                "<form action='hello'>" +                       // Submits a GET request to /hello - Uses Default GET request due to no method specified
+//                "<form action='hello'>" +                        // Submits a GET request to /hello - Uses Default GET request due to no method specified
                 "<form action='hello' method='post'?>" +           // Submits a POST request to /hello
                 "<input type='text' name='name'>" +
                 "<input type='submit' value='Greet me!'>" +
@@ -82,6 +84,5 @@ public class HelloController {
                 "</body>" +
                 "</html>";
     }
-
 
 }
